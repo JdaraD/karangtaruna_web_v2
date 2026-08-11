5b2cef99b1d9661728d640030b20dcfd8ca07e7f
Julian Diwandara <juliandara17@gmail.com>
2026-08-11 16:34:54 +0700

    update

--- resources/views/pages/admin/⚡about-us.blade.php
+++ resources/views/pages/admin/⚡about-us.blade.php
@@ -16,7 +16,7 @@ new class extends Component
 };
 ?>
 
-<section class="flex flex-col gap-4 w-full h-157.5 overflow-y-auto scrollbar-none">
+<section class="flex flex-col gap-4 w-full xl:h-200 lg:h-157.5 h-full overflow-y-auto scrollbar-none">
     <article class="flex flex-none gap-2 items-center">
         <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/></svg>
         <h1 class="font-semibold capitalize lg:text-2xl md:text-base text-base">Tentang Kami</h1>
--- resources/views/pages/admin/⚡struktur.blade.php
+++ resources/views/pages/admin/⚡struktur.blade.php
@@ -15,7 +15,7 @@ new class extends Component
 };
 ?>
 
-<section class="flex flex-col gap-4 w-full h-157.5 overflow-y-auto scrollbar-none">
+<section class="flex flex-col gap-4 w-full xl:h-200 lg:h-157.5 h-full overflow-y-auto scrollbar-none">
     <!-- Header Bagian Struktur -->
     <article class="flex flex-none gap-2 items-center">
         <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/></svg>
@@ -49,7 +49,7 @@ new class extends Component
 
     <!-- 2. Bagian Informasi Pengurus & Kartu Ketua -->
     <article class="flex flex-wrap w-full gap-4 items-center">
-        <div class="flex flex-col justify-stretch items-center w-[39%] gap-4 p-4 lg:h-76 h-auto bg-white rounded-md shadow-md">
+        <div class="flex flex-col justify-stretch items-center lg:w-[39%] w-full gap-4 p-4  lg:h-76 h-auto bg-white rounded-md shadow-md">
             <div class="flex w-full h-auto gap-1 justify-between items-center">
                 <div class="flex flex-col">
                     <p class="font-semibold text-base text-black capitalize">Pengurus Karang Taruna</p>
@@ -75,11 +75,11 @@ new class extends Component
                         <img src="{{ asset('img/foto.jpg') }}" alt="Ketua" class="w-full h-full object-contain rounded-md">
                     </div>
                     <div class="w-full h-[10%] flex flex-col justify-center items-center bg-gray-200 rounded-b-md">
-                        <p class="font-semibold text-sm normal-case">Ketua</p>
+                        <p class="text-black font-semibold text-sm normal-case">Sekretaris</p>
                     </div>
-                    <div class="absolute top-0 left-0 w-full h-full bg-gray-400 bg-opacity-90 opacity-0 hover:opacity-90 duration-150 transition-opacity ease-in-out rounded-md">
+                    <div class="absolute top-0 left-0 w-full h-full bg-gray-400 bg-opacity-90 opacity-0 hover:opacity-90 duration-150 transition-opacity ease-in-out rounded-md z-10">
                         <div class="flex flex-col w-full h-full justify-center items-center gap-2 p-2 whitespace-normal">
-                            <p class="font-semibold text-base text-black normal-case">Nama Ketua</p>
+                            <p class="font-semibold lg:text-base text-xs text-black normal-case">Nama Pengurus</p>
                             <p class="text-black font-normal text-xs text-center normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                         </div>
                     </div>
@@ -88,41 +88,44 @@ new class extends Component
         </div>
 
         <!-- 3. Bagian List Pengurus (Sekretaris, dll) -->
-            <div class="flex flex-col justify-stretch items-center w-[59.7%] gap-4 p-4 lg:h-76 h-auto bg-white rounded-md shadow-md">
-                <div class="flex w-full h-auto gap-1 justify-between items-center">
-                    <h1 class="font-semibold text-base text-black capitalize">Daftar Anggota / Pengurus Lainnya</h1>
-                    <div class="flex w-auto gap-1 justify-end items-center">
-                        <div class="flex bg-yellow-500 hover:bg-yellow-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer">
-                            <x-css-eye class="h-4 w-4 text-white"/>
-                        </div>
-                        <div class="flex bg-green-500 hover:bg-green-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer">
-                            <x-bi-plus class="h-6 w-6 text-white"/>
-                        </div>
-                        <div class="flex bg-red-500 hover:bg-red-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer">
-                            <x-bi-trash class="h-4 w-4 text-white"/>
-                        </div>
+        <div class="flex flex-col justify-stretch items-center lg:w-[59.7%] w-full gap-4 p-4 lg:h-76 h-auto bg-white rounded-md shadow-md">
+            <div class="flex w-full h-auto gap-1 justify-between items-center">
+                <h1 class="font-semibold text-base text-black capitalize">Daftar Anggota / Pengurus Lainnya</h1>
+                <div class="flex w-auto gap-1 justify-end items-center">
+                    <div class="flex bg-yellow-500 hover:bg-yellow-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer">
+                        <x-css-eye class="h-4 w-4 text-white"/>
+                    </div>
+                    <div class="flex bg-green-500 hover:bg-green-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer">
+                        <x-bi-plus class="h-6 w-6 text-white"/>
+                    </div>
+                    <div class="flex bg-red-500 hover:bg-red-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer">
+                        <x-bi-trash class="h-4 w-4 text-white"/>
                     </div>
                 </div>
-                
-                <div class="flex lg:w-183.75 w-full h-full justify-start items-center gap-4 scrollbar-thin overflow-x-auto p-2 rounded-md">
-                    @for ($i = 1; $i <= 6; $i++)
+            </div>
+            
+            <div class="flex w-full h-full overflow-hidden">
+                <div class="flex xl:w-240 lg:w-183.75 md:w-screen w-74 h-full justify-start items-center gap-4 scrollbar-thin overflow-x-auto p-2 rounded-md">
+                    @for ($i = 1; $i <= 8; $i++)
                     <div class="relative flex flex-none flex-col lg:w-38 md:w-32 w-22 lg:h-48 md:h-42 h-42 rounded-md shadow-md hover:scale-105 duration-150 transition-transform ease-in-out bg-white border border-gray-200">
                         <div class="w-full h-[90%] flex items-center justify-center p-2">
                             <img src="{{ asset('img/foto.jpg') }}" alt="Pengurus" class="w-full h-full object-contain rounded-md">
                         </div>
                         <div class="w-full h-[10%] flex flex-col justify-center items-center bg-gray-200 rounded-b-md">
-                            <p class="font-semibold text-sm normal-case">Sekretaris</p>
+                            <p class="text-black font-semibold text-sm normal-case">Sekretaris</p>
                         </div>
                         <div class="absolute top-0 left-0 w-full h-full bg-gray-400 bg-opacity-90 opacity-0 hover:opacity-90 duration-150 transition-opacity ease-in-out rounded-md z-10">
                             <div class="flex flex-col w-full h-full justify-center items-center gap-2 p-2 whitespace-normal">
-                                <p class="font-semibold text-base text-black normal-case">Nama Pengurus</p>
+                                <p class="font-semibold lg:text-base text-xs text-black normal-case justify-center">Nama Pengurus</p>
                                 <p class="text-black font-normal text-xs text-center normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                             </div>
                         </div>
                     </div>
                     @endfor
                 </div>
+
             </div>
+        </div>
     </article>
 
 </section>
