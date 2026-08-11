35584455b2918cbd20dcf5e2fb02a0fdf39db8ef
Julian Diwandara <juliandara17@gmail.com>
2026-08-11 13:59:16 +0700

    update page dashboard admin interface

--- resources/views/pages/admin/⚡dashboard.blade.php
+++ resources/views/pages/admin/⚡dashboard.blade.php
@@ -14,6 +14,29 @@ new class extends Component
 };
 ?>
 
-<div class="flex w-full h-full bg-white shadow-md rounded-lg items-center justify-center ">
-    <p class="text-gray-800">page admin dashboard</p>
-</div>
+<section class="flex flex-col gap-4 w-full h-157.5 overflow-y-auto scrollbar-none">
+    <article class="flex flex-none gap-2 items-center">
+        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 00-1-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/></svg>
+        <h1 class="font-semibold capitalize lg:text-2xl md:text-base text-base ">Dashboard</h1>
+    </article>
+
+    <article class="flex flex-none gap-4 items-center justify-between w-full">
+        @for ($i = 1; $i <= 3; $i++)
+        <div class="flex w-full lg:h-34 md:h-30 h-18 bg-gray-300 animate-pulse shadow-lg"></div>
+        @endfor
+    </article>
+
+    <article class="flex flex-none gap-4 items-center w-full">
+        <div class="flex justify-center items-center w-[64%] h-80 bg-gray-300 animate-pulse shadow-lg">
+            <p class="text-black">statistik view</p>
+        </div>
+        <div class="flex justify-center items-center w-[36%] h-80 bg-gray-300 animate-pulse shadow-lg">
+            <p class="text-black">profile perusahan</p>
+        </div>
+    </article>
+
+    <article class="flex flex-none w-full h-100 bg-gray-300 animate-pulse shadow-lg">
+
+    </article>
+
+</section>
--- resources/views/pages/user/⚡detail-kolaborasi.blade.php
+++ resources/views/pages/user/⚡detail-kolaborasi.blade.php
@@ -22,15 +22,58 @@ new class extends Component
             <a href="{{ route('kolaborasi') }}" class="font-[poppins] font-semibold lg:text-2xl md:text-lg text-base normal-case">Kolaborasi :</a>
             <p class="font-[poppins] font-normal lg:text-2xl md:text-lg text-base normal-case">Program 1</p>
         </div>
+    </article>
 
-        <div class="flex flex-wrap w-full h-full justify-center items-center lg:gap-4 md:gap-4 gap-2">
-            @for ($i = 1; $i <= 8; $i++)
-                <div class="flex relative flex-col lg:w-82 md:w-80 w-30 lg:h-60 md:h-58 h-28 bg-gray-300 rounded-lg shadow-md hover:scale-102 transition-transform duration-120 ease-in-out">
-                    <div class="flex w-full h-full rounded-t-lg">
-                        <img src="{{ asset('img/program.jpg') }}" alt="" class="w-full h-full object-cover rounded-t-lg">
+    <article class="flex flex-wrap justify-center w-[90%] h-full gap-6">
+        <div class="flex flex-col lg:w-[58%] md:w-[58%] w-full h-full gap-6 lg:order-1 md:order-1 order-2">
+    
+            <div class="flex flex-col w-full h-full gap-6">
+                @for ($i = 1; $i <= 8; $i++)
+                    <div class="flex w-full h-full gap-4 bg-gray-100 rounded-lg shadow-md p-4 hover:scale-102 transition-transform duration-120 ease-in-out">
+                        <div class="flex lg:w-90 md:w-80 w-60 lg:h-40 md:h-30 h-20 bg-gray-400 animate-pulse rounded-lg">
+                            {{-- <img src="" alt="" class="w-full h-full object-cover rounded-lg"> --}}
+                            
+                        </div>
+    
+                        <div class="flex flex-col w-full h-full gap-2">
+                            <h1 class="font-[poppins] font-semibold lg:text-lg md:text-base text-sm normal-case">News 1</h1>
+                            <p class="font-[poppins] lg:text-base md:text-sm text-xs text-justify lg:line-clamp-0 md:line-clamp-0 line-clamp-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+    
+                        </div>
+                    </div>
+                @endfor
+            </div>
+    
+        </div>
+    
+        <div class="flex flex-col lg:w-[39%] md:w-[39%] w-full h-full gap-6 lg:order-2 md:order-2 order-1">
+            <div class="flex flex-col w-full h-80 gap-2 bg-gray-300 animate-pulse rounded-lg">
+               
+            </div>
+
+            <div class="lg:flex md:flex hidden flex-col w-[90%] h-full gap-2">
+                <h1 class="font-[poppins] font-semibold lg:text-lg md:text-base text-sm normal-case">Events</h1>
+                <div class="border border-b-gray-300"></div>
+            </div>
+
+            <div class="lg:flex md:flex hidden flex-col w-full h-full gap-6">
+                @for ($i = 1; $i <= 4; $i++)
+
+                <div class="flex w-full h-full gap-4 bg-gray-100 rounded-lg shadow-md p-4 hover:scale-102 transition-transform duration-120 ease-in-out">
+                    <div class="flex lg:w-48 md:w-40 w-32 lg:h-20 md:h-16 h-12 bg-gray-400 animate-pulse rounded-lg">
+                        {{-- <img src="" alt="" class="w-full h-full object-cover rounded-lg"> --}}
+                        
+                    </div>
+
+                    <div class="flex flex-col w-full h-full gap-2">
+                        <h1 class="font-[poppins] font-semibold lg:text-base md:text-sm text-xs normal-case">Event 1</h1>
+                        <p class="font-[poppins] lg:text-base md:text-sm text-xs text-justify lg:line-clamp-0 md:line-clamp-0 line-clamp-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+
                     </div>
                 </div>
-            @endfor
+                @endfor
+            </div>
+
         </div>
 
     </article>
