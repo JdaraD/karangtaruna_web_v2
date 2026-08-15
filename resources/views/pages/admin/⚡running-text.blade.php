abecbea6d6d351575bceb6c16259a333e34f54c5
Julian Diwandara <juliandara17@gmail.com>
2026-08-15 22:05:48 +0700

    fix bug in page running text admin panel

--- resources/views/pages/admin/⚡running-text.blade.php
+++ resources/views/pages/admin/⚡running-text.blade.php
@@ -4,17 +4,18 @@ use Livewire\Component;
 
 new class extends Component
 {
-    public $ovelayAdd = false;
+    public $overlayAdd = false;
 
     public function btnOpenAdd()
     {
         $this->overlayAdd = true;
     }
 
-    public function closeOpenAdd()
+    public function btnCloseAdd()
     {
         $this->overlayAdd = false;
     }
+
     
     public function render()
     {
@@ -40,7 +41,7 @@ new class extends Component
                     <h1 class="font-semibold text-base text-black capitalize">Running Text</h1>
                 </div>
                 <div class="flex w-full h-auto gap-1 justify-end items-center">
-                    <button id="btbOpenAdd" name="btnOpenAdd" class="flex bg-green-500 hover:bg-green-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Tambah/Edit">
+                    <button wire:click="btnOpenAdd" class="flex bg-green-500 hover:bg-green-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Tambah/Edit">
                         <x-bi-plus class="h-6 w-6 text-white"/>
                     </button>
                 </div>
@@ -68,15 +69,50 @@ new class extends Component
     </article>
 
     {{-- overlay btn add --}}
-    <article id="overlayAdd" name="overlayAdd" class="absolute items-center justify-center top-0 left-0 w-full h-full bg-gray-400/60 z-50">
-        <div class="relative flex w-[80%] h-[80%] items-center justify-center bg-white">
-            <div id="btnCloseAdd" class="absolute top-2 right-2 rounded-md p-2 bg-amber-200">
-                <p>close</p>
-            </div>
-            <h1 class="text-2xl font-semibold text-black">text</h1>
+    @if ($overlayAdd)
+        <article class="absolute flex inset-0 items-center justify-center w-full h-full bg-gray-400/60 z-50">
+            <div class="flex flex-col w-fit h-fit gap-4 p-4 bg-white rounded-md">
+                
+                <div class="flex w-full h-fit gap-1 justify-between items-center bg-gray-100 rounded-md p-2">
+                    <div class="flex w-full h-auto gap-1 items-center">
+                        <h1 class="font-semibold text-base text-black capitalize">Tambah Running Text</h1>
+                    </div>
+                    <div class="flex w-[30%] h-auto gap-1 justify-end items-center">
+                        <button wire:click="btnCloseAdd" class=" top-4 right-4 rounded-full p-1 bg-red-500 hover:bg-red-700 cursor-pointer">
+                            <x-css-close class="w-3 h-3" />
+                        </button>
+                    </div>
+                </div>
 
-        </div>
-    </article>
+                <div class="flex flex-col w-full gap-5 pt-2">
+
+                    <div class="grid grid-cols-1 md:grid-cols-4 items-center gap-2">
+                        <label for="judul" class="text-sm font-semibold text-gray-800">
+                            Judul Berita
+                        </label>
+
+                        <input type="text" name="judul" id="judul" placeholder="Masukkan judul berita" class="md:col-span-3 w-full rounded-md border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+                    </div>
+
+                    <div class="grid grid-cols-1 md:grid-cols-4 items-start gap-2">
+                        <label for="berita" class="text-sm font-semibold text-gray-800 pt-2">
+                            Berita
+                        </label>
+
+                        <textarea name="berita" id="berita" rows="7" placeholder="Masukkan isi berita" class="md:col-span-3 w-full resize-none rounded-md border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200"></textarea>
+                    </div>
+
+                </div>
+
+                <div class="flex w-full h-full justify-end items-end">
+                    <button wire:click="btnAdd" class="flex justify-center items-center p-2 rounded-md bg-green-500 hover:bg-green-700 shadow-md cursor-pointer">
+                        Tambah
+                    </button>
+                </div>
+            </div>
+        </article>
+        
+    @endif
     {{-- overlay btn add --}}
     
 </section>
