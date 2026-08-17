47e12ccd07100c261d5e4f735b2f42963a9d9d9c
Julian Diwandara <juliandara17@gmail.com>
2026-08-17 13:51:51 +0700

    update fungsi update for page running text in admin panel

--- resources/views/pages/admin/⚡running-text.blade.php
+++ resources/views/pages/admin/⚡running-text.blade.php
@@ -6,9 +6,15 @@ use App\Models\runningText;
 new class extends Component
 {
     public $overlayAdd = false;
+    public $overlayEdit = false;
     public $runningText;
+    public $runningTextId;
+    public $judul;
+    public $text;
     public $deleteSuccess = '';
     public $deleteGagal = '';
+    public $editSuccess = '';
+    public $editGagal = '';
 
     public function loadRunningText()
     {
@@ -29,6 +35,57 @@ new class extends Component
     public function btnCloseAdd()
     {
         $this->overlayAdd = false;
+    }
+
+    public function btnOpenEdit($id)
+    {
+        $runningText = runningText::findOrFail($id);
+        
+        $this->runningTextId = $runningText->id;
+        $this->judul = $runningText->judul;
+        $this->text = $runningText->text;
+
+        $this->overlayEdit = true;
+    }
+
+    public function btnCloseEdit()
+    {
+        $this->overlayEdit = false;
+        $this->reset([
+        'runningTextId',
+        'judul',
+        'text',
+    ]);
+
+    $this->resetValidation();
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'text' => 'required',
+            'judul' => 'required'
+        ]);
+
+        try {
+            $runningText = RunningText::findOrFail(
+                $this->runningTextId
+            );
+            $runningText->update([
+                'text' => $this->text,
+                'judul' => $this->judul
+            ]);
+    
+            $this->loadRunningText();
+            
+            $this->editSuccess = 'Data Berhasil Diedit!';
+            $this->editGagal = '';
+
+            $this->overlayEdit = false;
+        } catch (\Throwable $th) {
+            $this->editGagal = 'Data Gagal Diedit!';
+            $this->editSuccess = 'Data Gagal Diedit!';
+        }
     }
 
     public function btnDelete($id)
@@ -92,9 +149,9 @@ new class extends Component
                         <div class="flex w-full h-8 gap-1 p-1 justify-between items-center bg-[#618764]/40 rounded-md">
                             <p class="text-base font-semibold capitalize">{{$text->judul}}</p>
                             <div class="flex gap-1">
-                                <div class="flex bg-yellow-500 hover:bg-yellow-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Lihat">
+                                <button type="button" wire:click="btnOpenEdit({{ $text->id }})" class="flex bg-yellow-500 hover:bg-yellow-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Lihat">
                                     <x-bi-pencil class="h-4 w-4 text-white"/>
-                                </div>
+                                </button>
                                 <button type="button" wire:click="btnDelete({{ $text->id }})" class="flex bg-red-500 hover:bg-red-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Hapus">
                                     <x-bi-trash class="h-4 w-4 text-white"/>
                                 </button>
@@ -154,6 +211,54 @@ new class extends Component
     @endif
     {{-- overlay btn add --}}
 
+    {{-- overlay Edit --}}
+    @if ($overlayEdit)
+        <article class="absolute flex top-0 left-0 items-center justify-center w-full h-full bg-gray-400/60 z-50">
+            <form wire:submit.prevent="update" class="flex flex-col w-fit h-fit gap-4 p-4 bg-white rounded-md">
+                
+                <div class="flex w-full h-fit gap-1 justify-between items-center bg-gray-100 rounded-md p-2">
+                    <div class="flex w-full h-auto gap-1 items-center">
+                        <h1 class="font-semibold text-base text-black capitalize">Edit Running Text</h1>
+                    </div>
+                    <div class="flex w-[30%] h-auto gap-1 justify-end items-center">
+                        <button type="button" wire:click="btnCloseEdit" class=" top-4 right-4 rounded-full p-1 bg-red-500 hover:bg-red-700 cursor-pointer">
+                            <x-css-close class="w-3 h-3" />
+                        </button>
+                    </div>
+                </div>
+
+                <div class="flex flex-col w-full gap-5 pt-2">
+
+                    <div class="grid grid-cols-1 md:grid-cols-4 items-center gap-2">
+                        <label for="judul" class="text-sm font-semibold text-gray-800">
+                            Judul Berita
+                        </label>
+
+                        <input type="text" wire:model="judul" name="judul" id="judul" placeholder="Masukkan judul berita" class="md:col-span-3 w-full rounded-md text-black border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+                    </div>
+
+                    <div class="grid grid-cols-1 md:grid-cols-4 items-start gap-2">
+                        <label for="berita" class="text-sm font-semibold text-gray-800 pt-2">
+                            Berita
+                        </label>
+
+                        <textarea name="text" wire:model="text" id="text" rows="7" placeholder="Masukkan isi berita" class="md:col-span-3 w-full resize-none rounded-md text-black border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200"></textarea>
+                    </div>
+
+                </div>
+
+                <div class="flex w-full h-full justify-end items-end">
+                    <button type="submit" class="flex justify-center items-center p-2 rounded-md bg-green-500 hover:bg-green-700 shadow-md cursor-pointer">
+                        Edit
+                    </button>
+                </div>
+            </form>
+            
+        </article>
+        
+    @endif
+    {{-- overlay Edit --}}
+
     {{-- notifikasi Add --}}
     @if (session('addSuccess'))
         <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show" x-transition.duration.500ms class="absolute top-2 right-6 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4" role="alert">
@@ -181,5 +286,19 @@ new class extends Component
         </div>
     @endif
     {{-- notifikasi delete --}}
+
+    {{-- notifikasi Edit --}}
+    @if ($editSuccess)
+        <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show" x-transition.duration.500ms class="absolute top-2 right-6 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4" role="alert">
+            <span class="block sm:inline">{{ $editSuccess }}</span>
+        </div>
+    @endif
+
+    @if ($editGagal)
+        <div class="absolute top-2 right-6 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4" role="alert">
+            <span class="block sm:inline">{{ $editGagal }}</span>
+        </div>
+    @endif
+    {{-- notifikasi Edit --}}
     
 </section>
