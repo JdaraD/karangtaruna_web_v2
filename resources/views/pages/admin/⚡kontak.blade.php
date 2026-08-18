baf30eeb3ca73961b7270868c8be26fa7a656d03
Julian Diwandara <juliandara17@gmail.com>
2026-08-18 11:36:37 +0700

    update page bantuan function update and delete in section kontak bantuan

--- resources/views/pages/admin/⚡kontak.blade.php
+++ resources/views/pages/admin/⚡kontak.blade.php
@@ -6,12 +6,13 @@ use App\Models\KontakBantuan;
 
 new class extends Component
 {
+    public $name, $gmail, $no_hp, $wilayah;
+
     public $kontakAdmin;
     public $kontakAdminId;
-    public $name, $gmail, $no_hp;
 
-    public $kontakBantuan;
-    public $kontakBantuanId;
+    public $KontakBantuan;
+    public $KontakBantuanId;
 
     public $overlayAdmin = false;
     public $overlayEditAdmin = false;
@@ -31,7 +32,7 @@ new class extends Component
     
     public function loadKontakBantuan()
     {
-        $this->kontakBantuan = KontakBantuan::get();
+        $this->KontakBantuan = KontakBantuan::get();
 
     }
     // laod data
@@ -85,6 +86,29 @@ new class extends Component
             'no_hp',
         ]);
     }
+
+    public function btnOpenEditBantuan($id)
+    {
+        $KontakBantuan = KontakBantuan::findOrFail($id);
+
+        $this->KontakBantuanId = $KontakBantuan->id;
+        $this->wilayah = $KontakBantuan->wilayah;
+        $this->name = $KontakBantuan->name;
+        $this->no_hp = $KontakBantuan->no_hp;
+
+        $this->overlayEditBantuan = true;
+    }
+
+    public function btnCloseEditBantuan()
+    {
+        $this->overlayEditBantuan = false;
+        $this->reset([
+            'KontakBantuanId',
+            'wilayah',
+            'name',
+            'no_hp'
+        ]);
+    }
     // function button
 
     // function Update
@@ -118,6 +142,38 @@ new class extends Component
             
         }
     }
+
+    public function updateBantuan()
+    {
+        $this->validate([
+            'wilayah' => 'required',
+            'name' => 'required',
+            'no_hp' => 'required'
+        ]);
+
+        try {
+            $KontakBantuan = KontakBantuan::findOrFail(
+                $this->KontakBantuanId
+            );
+
+            $KontakBantuan->update([
+                'wilayah' => $this->wilayah,
+                'name' => $this->name,
+                'no_hp' => $this->no_hp
+            ]);
+
+            $this->loadKontakBantuan();
+
+            $this->editSuccess = 'Data Berhasil Diedit!';
+            $this->editGagal = '';
+            $this->overlayEditBantuan = false;
+        } catch (\Throwable $th) {
+            // $this->editGagal = 'Data Gagal Diedit!';
+            // $this->editSuccess = '';
+            dd($th->getMessage());
+
+        }
+    }
     // function Update
 
     // function Delete
@@ -128,6 +184,22 @@ new class extends Component
             $kontakAdmin->delete();
 
             $this->loadKontakAdmin();
+
+            $this->deleteSuccess = 'Data Berhasil Dihapus!';
+            $this->deleteGagal = '';
+        } catch (\Throwable $th) {
+            $this->deleteGagal = 'Data Gagal Dihapus!';
+            $this->deleteSuccess = '';
+        }
+    }
+
+    public function btnDeleteBantuan($id)
+    {
+        try {
+            $KontakBantuan = KontakBantuan::findOrFail($id);
+            $KontakBantuan->delete();
+
+            $this->loadKontakBantuan();
 
             $this->deleteSuccess = 'Data Berhasil Dihapus!';
             $this->deleteGagal = '';
@@ -210,16 +282,16 @@ new class extends Component
             </div>
 
             <div class="grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 w-full 3xl:h-70 lg:h-40 md:h-40 h-56 gap-2 p-2 overflow-y-auto scrollbar-none">
-                @foreach ($kontakBantuan as $kb)
+                @foreach ($KontakBantuan as $kb)
                     <div class="flex w-full max-h-26 h-full gap-2 p-2 bg-[#9CB080] rounded-md shadow-md hover:scale-102 duration-120 ease-in-out transition-transform">
                         <div class="flex w-full h-full flex-col gap-1">
                             <div class="flex gap-1 p-1 justify-between items-center bg-[#618764]/40 rounded-md">
                                 <p class="text-base font-semibold capitalize">{{ $kb->wilayah }}</p>
                                 <div class="flex gap-1">
-                                    <button wire:click="btnOpenEditbantuan({{ $kb->id }})" class="flex bg-yellow-500 hover:bg-yellow-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Lihat">
+                                    <button wire:click="btnOpenEditBantuan({{ $kb->id }})" class="flex bg-yellow-500 hover:bg-yellow-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Lihat">
                                         <x-bi-pencil class="h-4 w-4 text-white"/>
                                     </button>
-                                    <button wire:click= class="flex bg-red-500 hover:bg-red-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Hapus">
+                                    <button wire:click="btnDeleteBantuan({{ $kb->id }})" class="flex bg-red-500 hover:bg-red-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Hapus">
                                         <x-bi-trash class="h-4 w-4 text-white"/>
                                     </button>
                                 </div>
@@ -414,6 +486,66 @@ new class extends Component
         
     @endif
     {{-- overlayAdd Bantuan --}}
+
+    {{-- overlayEdit Bantuan--}}
+    @if ($overlayEditBantuan)
+        <article class="absolute flex top-0 left-0 items-center justify-center w-full h-full bg-gray-400/60 z-50">
+            <div class="flex flex-col w-fit h-fit gap-4 p-4 bg-white rounded-md">
+                
+                <div class="flex w-full h-fit gap-1 justify-between items-center bg-gray-100 rounded-md p-2">
+                    <div class="flex w-full h-auto gap-1 items-center">
+                        <h1 class="font-semibold text-base text-black capitalize">Edit Kontak Bantuan</h1>
+                    </div>
+                    <div class="flex w-[30%] h-auto gap-1 justify-end items-center">
+                        <button type="button" wire:click="btnCloseEditBantuan" class=" top-4 right-4 rounded-full p-1 bg-red-500 hover:bg-red-700 cursor-pointer">
+                            <x-css-close class="w-3 h-3" />
+                        </button>
+                    </div>
+                </div>
+
+                <form wire:submit.prevent="updateBantuan" class="flex flex-col gap-4">
+                    @csrf
+                    
+                    <div class="flex flex-col w-full gap-5 pt-2">
+    
+                        <div class="grid grid-cols-1 md:grid-cols-4 items-center gap-2">
+                            <label for="wilayah" class="text-sm font-semibold text-gray-800">
+                                Wilayah
+                            </label>
+    
+                            <input type="text" wire:model="wilayah" name="wilayah" id="wilayah" placeholder="Masukkan Nama Wilayah (RW 01)" class="md:col-span-3 w-full rounded-md text-black border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+                        </div>
+    
+                        <div class="grid grid-cols-1 md:grid-cols-4 items-start gap-2">
+                            <label for="name" class="text-sm font-semibold text-gray-800 pt-2">
+                                Nama
+                            </label>
+    
+                           <input type="text" wire:model="name" name="name" id="name" placeholder="Masukkan Nama" class="md:col-span-3 w-full rounded-md text-black border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+                        </div>
+    
+                        <div class="grid grid-cols-1 md:grid-cols-4 items-start gap-2">
+                            <label for="no_hp" class="text-sm font-semibold text-gray-800 pt-2">
+                                Nomor Hp
+                            </label>
+    
+                           <input type="text" wire:model="no_hp" name="no_hp" id="no_hp" placeholder="Masukkan Nomor Hp (08xxxxxxxx)" oninput="this.value = this.value.replace(/[^0-9]/g, '')" maxlength="20" inputmode="numeric" class="md:col-span-3 w-full rounded-md text-black border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+                        </div>
+    
+                    </div>
+    
+                    <div class="flex w-full h-full justify-end items-end">
+                        <button type="submit" class="flex justify-center items-center p-2 rounded-md bg-green-500 hover:bg-green-700 shadow-md cursor-pointer">
+                            Edit
+                        </button>
+                    </div>
+                </form>
+            </div>
+            
+        </article>
+        
+    @endif
+    {{-- overlayEdit Bantuan --}}
 
         {{-- notifikasi Add --}}
     @if (session('addSuccess'))
