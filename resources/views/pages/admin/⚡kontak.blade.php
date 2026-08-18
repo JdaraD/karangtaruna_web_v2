2ca051d956c51833698c42af2630bb25dc78555a
Julian Diwandara <juliandara17@gmail.com>
2026-08-18 11:05:26 +0700

    update page bantuan function update and delete in section kontak admin

--- resources/views/pages/admin/⚡kontak.blade.php
+++ resources/views/pages/admin/⚡kontak.blade.php
@@ -7,10 +7,16 @@ use App\Models\KontakBantuan;
 new class extends Component
 {
     public $kontakAdmin;
+    public $kontakAdminId;
+    public $name, $gmail, $no_hp;
+
     public $kontakBantuan;
+    public $kontakBantuanId;
 
     public $overlayAdmin = false;
+    public $overlayEditAdmin = false;
     public $overlayBantuan = false;
+    public $overlayEditBantuan = false;
 
     public $deleteSuccess;
     public $deleteGagal;
@@ -56,7 +62,81 @@ new class extends Component
     {
         $this->overlayBantuan = false;
     }
+
+    public function btnOpenEditAdmin($id)
+    {
+        $kontakAdmin = kontakAdmin::findOrFail($id);
+
+        $this->kontakAdminId = $kontakAdmin->id;
+        $this->name = $kontakAdmin->name;
+        $this->gmail = $kontakAdmin->gmail;
+        $this->no_hp = $kontakAdmin->no_hp;
+
+        $this->overlayEditAdmin = true;
+    }
+
+    public function btnCloseEditAdmin()
+    {
+        $this->overlayEditAdmin = false;
+        $this->reset([
+            'kontakAdminId',
+            'name',
+            'gmail',
+            'no_hp',
+        ]);
+    }
     // function button
+
+    // function Update
+    public function updateAdmin()
+    {
+        $this->validate([
+            'name' => 'required',
+            'gmail' => 'required',
+            'no_hp' => 'required'
+        ]);
+
+        try {
+            $kontakAdmin = kontakAdmin::findOrFail(
+                $this->kontakAdminId
+            );
+
+            $kontakAdmin->update([
+                'name' => $this->name,
+                'gmail' => $this->gmail,
+                'no_hp' => $this->no_hp
+            ]);
+
+            $this->loadKontakAdmin();
+
+            $this->editSuccess = 'Data Berhasil Diedit!';
+            $this->editGagal = '';
+            $this->overlayEditAdmin = false;
+        } catch (\Throwable $th) {
+            $this->editGagal = 'Data Gagal Diedit!';
+            $this->editSuccess = '';
+            
+        }
+    }
+    // function Update
+
+    // function Delete
+    public function btndeleteAdmin($id)
+    {
+        try {
+            $kontakAdmin = kontakAdmin::findOrFail($id);
+            $kontakAdmin->delete();
+
+            $this->loadKontakAdmin();
+
+            $this->deleteSuccess = 'Data Berhasil Dihapus!';
+            $this->deleteGagal = '';
+        } catch (\Throwable $th) {
+            $this->deleteGagal = 'Data Gagal Dihapus!';
+            $this->deleteSuccess = '';
+        }
+    }
+    // function Delete
     
     public function render()
     {
@@ -95,12 +175,12 @@ new class extends Component
                             <div class="flex gap-1 p-1 justify-between items-center bg-[#618764]/40 rounded-md">
                                 <p class="text-base font-semibold capitalize">{{ $ka->name }}</p>
                                 <div class="flex gap-1">
-                                    <div class="flex bg-yellow-500 hover:bg-yellow-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Lihat">
+                                    <button wire:click="btnOpenEditAdmin({{ $ka->id }})" class="flex bg-yellow-500 hover:bg-yellow-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Lihat">
                                         <x-bi-pencil class="h-4 w-4 text-white"/>
-                                    </div>
-                                    <div class="flex bg-red-500 hover:bg-red-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Hapus">
+                                    </button>
+                                    <button wire:click="btndeleteAdmin({{ $ka->id }})" class="flex bg-red-500 hover:bg-red-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Hapus">
                                         <x-bi-trash class="h-4 w-4 text-white"/>
-                                    </div>
+                                    </button>
                                 </div>
                             </div>
                             <div class="flex gap-2 items-center">
@@ -136,12 +216,12 @@ new class extends Component
                             <div class="flex gap-1 p-1 justify-between items-center bg-[#618764]/40 rounded-md">
                                 <p class="text-base font-semibold capitalize">{{ $kb->wilayah }}</p>
                                 <div class="flex gap-1">
-                                    <div class="flex bg-yellow-500 hover:bg-yellow-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Lihat">
+                                    <button wire:click="btnOpenEditbantuan({{ $kb->id }})" class="flex bg-yellow-500 hover:bg-yellow-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Lihat">
                                         <x-bi-pencil class="h-4 w-4 text-white"/>
-                                    </div>
-                                    <div class="flex bg-red-500 hover:bg-red-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Hapus">
+                                    </button>
+                                    <button wire:click= class="flex bg-red-500 hover:bg-red-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Hapus">
                                         <x-bi-trash class="h-4 w-4 text-white"/>
-                                    </div>
+                                    </button>
                                 </div>
                             </div>
                             <div class="flex gap-2 items-center">
@@ -215,6 +295,66 @@ new class extends Component
         
     @endif
     {{-- overlayAdd Admin --}}
+
+    {{-- overlayEdit Admin --}}
+    @if ($overlayEditAdmin)
+        <article class="absolute flex top-0 left-0 items-center justify-center w-full h-full bg-gray-400/60 z-50">
+            <div class="flex flex-col w-fit h-fit gap-4 p-4 bg-white rounded-md">
+                
+                <div class="flex w-full h-fit gap-1 justify-between items-center bg-gray-100 rounded-md p-2">
+                    <div class="flex w-full h-auto gap-1 items-center">
+                        <h1 class="font-semibold text-base text-black capitalize">Edit Kontak Admin</h1>
+                    </div>
+                    <div class="flex w-[30%] h-auto gap-1 justify-end items-center">
+                        <button type="button" wire:click="btnCloseEditAdmin" class=" top-4 right-4 rounded-full p-1 bg-red-500 hover:bg-red-700 cursor-pointer">
+                            <x-css-close class="w-3 h-3" />
+                        </button>
+                    </div>
+                </div>
+
+                <form wire:submit.prevent="updateAdmin" class="flex flex-col gap-4">
+                    @csrf
+                    
+                    <div class="flex flex-col w-full gap-5 pt-2">
+    
+                        <div class="grid grid-cols-1 md:grid-cols-4 items-center gap-2">
+                            <label for="name" class="text-sm font-semibold text-gray-800">
+                                Nama admin
+                            </label>
+    
+                            <input type="text" wire:model="name" name="name" id="name" placeholder="Masukkan Nama Admin" class="md:col-span-3 w-full rounded-md text-black border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+                        </div>
+    
+                        <div class="grid grid-cols-1 md:grid-cols-4 items-start gap-2">
+                            <label for="gmail" class="text-sm font-semibold text-gray-800 pt-2">
+                                Gmail
+                            </label>
+    
+                           <input type="text" name="gmail" wire:model="gmail" id="gmail" placeholder="Masukkan Gmail" class="md:col-span-3 w-full rounded-md text-black border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+                        </div>
+    
+                        <div class="grid grid-cols-1 md:grid-cols-4 items-start gap-2">
+                            <label for="no_hp" class="text-sm font-semibold text-gray-800 pt-2">
+                                Nomor Hp
+                            </label>
+    
+                           <input type="text" name="no_hp" id="no_hp" wire:model="no_hp" placeholder="Masukkan Nomor Hp (08xxxxxxxx)" oninput="this.value = this.value.replace(/[^0-9]/g, '')" maxlength="20" inputmode="numeric" class="md:col-span-3 w-full rounded-md text-black border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+                        </div>
+    
+                    </div>
+    
+                    <div class="flex w-full h-full justify-end items-end">
+                        <button type="submit" class="flex justify-center items-center p-2 rounded-md bg-green-500 hover:bg-green-700 shadow-md cursor-pointer">
+                            Edit
+                        </button>
+                    </div>
+                </form>
+            </div>
+            
+        </article>
+        
+    @endif
+    {{-- overlayEdit Admin --}}
 
     {{-- overlayAdd Bantuan --}}
     @if ($overlayBantuan)
