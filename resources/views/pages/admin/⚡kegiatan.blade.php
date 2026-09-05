fb5df912734036b478fb21fd245c3cd16824d24c
Julian Diwandara <juliandara17@gmail.com>
2026-09-05 14:21:25 +0700

    create db kegitan

--- resources/views/pages/admin/⚡kegiatan.blade.php
+++ resources/views/pages/admin/⚡kegiatan.blade.php
@@ -1,9 +1,45 @@
 <?php
 
 use Livewire\Component;
+use app\models\kegiatan;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\UploadedFile;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\Encoders\WebpEncoder;
 
 new class extends Component
 {
+
+    public $kegiatan;
+
+    public $overlayAddKegiatan = false;
+    public $overlayEditKegiatan = false;
+
+    public $editSuccess;
+    public $editGagal;
+    public $deleteSuccess;
+    public $deleteGagal;
+
+    // load data
+
+    // load data
+
+    // function mount
+    // function mount
+
+    // function Button
+    // function Button
+
+    // add function
+    // add function
+
+    // update function
+    // update function
+
+    // delete function
+    // delete function
     
     public function render()
     {
@@ -25,40 +61,241 @@ new class extends Component
         <div class="flex flex-col justify-stretch gap-4 items-center w-full h-auto p-4 bg-white shadow-md rounded-md">
             <div class="flex w-full h-auto gap-1 justify-between items-center bg-gray-100 rounded-md p-2">
                 <div class="flex w-full h-auto gap-1 items-center">
-                    <h1 class="font-semibold text-base text-black capitalize">Kegiatan Karang taruna/h1>
+                    <h1 class="font-semibold text-base text-black capitalize">Kegiatan Karang taruna</h1>
                 </div>
                 <div class="flex w-full h-auto gap-1 justify-end items-center">
-                    <div class="flex bg-green-500 hover:bg-green-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Tambah/Edit">
+                    <button type="button" wire:click="btnOpenAddKegiatan" class="flex bg-green-500 hover:bg-green-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Tambah/Edit">
                         <x-bi-plus class="h-6 w-6 text-white"/>
-                    </div>
+                    </button>
                 </div>
             </div>
 
             <div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 w-full h-auto gap-2">
-                @for ($i = 1; $i <= 12; $i++)
+                @foreach ($kegiatan as $ke)
                     <div class="flex flex-col w-full h-auto gap-2 p-2 bg-[#9CB080] rounded-md shadow-md hover:scale-102 duration-120 ease-in-out transition-transform">
                         <div class="flex gap-1 h-[79%]">
-                            <img src="{{ asset('img/mbg.jpg') }}" alt="" class="w-44 h-28 rounded-md">
+                            <img src="{{ asset('storage/' . $ke->gambar) }}" alt="" class="w-44 h-28 rounded-md">
                             <div class="flex flex-col gap-1">
                                 <div class="flex items-center gap-1">
                                     <p class="text-base font-semibold text-black">Program :</p>
-                                    <p class="text-base font-semibold text-black">MBG</p>
+                                    <p class="text-base font-semibold text-black">{{ $ke->judul }}</p>
                                 </div>
-                                <p class="text-xs line-clamp-4 text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae tempore beatae illo officia alias quia ullam odit cumque! Officiis quaerat atque placeat officia corrupti tempora ad natus cupiditate aspernatur facere.</p>
+                                <p class="text-xs line-clamp-4 text-justify">{{ $ke->deskripsi }}</p>
                             </div>
                         </div>
                         <div class="flex w-full h-[20%] gap-1 p-1 justify-end items-center bg-[#618764]/40 rounded-md">
-                            <div class="flex bg-yellow-500 hover:bg-yellow-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Lihat">
-                                <x-css-eye class="h-4 w-4 text-white"/>
-                            </div>
-                            <div class="flex bg-red-500 hover:bg-red-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Hapus">
+                            <button type="button" wire:click="btnOpenEditKegiatan({{ $ke->id }})" class="flex bg-yellow-500 hover:bg-yellow-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Lihat">
+                                <x-bi-pencil class="h-4 w-4 text-white"/>
+                            </button>
+                            <button type="button" wire:click="btnDeleteKegiatan({{ $ke->id }})" class="flex bg-red-500 hover:bg-red-700 justify-center items-center w-6 h-6 rounded-md shadow-md cursor-pointer" title="Hapus">
                                 <x-bi-trash class="h-4 w-4 text-white"/>
-                            </div>
+                            </button>
                         </div>
                     </div>
-                @endfor
+                @endforeach
             </div>
         </div>
     </article>
+
+    {{-- overlay Add Kegiatan--}}
+    @if ($overlayAddKegiatan)
+        <article class="absolute flex top-0 left-0 items-center justify-center w-full h-full bg-gray-400/60 z-50">
+            <div class="flex flex-col w-fit h-fit gap-4 p-4 bg-white rounded-md">
+                
+                <div class="flex w-full h-fit gap-1 justify-between items-center bg-gray-100 rounded-md p-2">
+                    <div class="flex w-full h-auto gap-1 items-center">
+                        <h1 class="font-semibold text-base text-black capitalize">Tambah Kegiatan</h1>
+                    </div>
+                    <div class="flex w-[30%] h-auto gap-1 justify-end items-center">
+                        <button wire:click="btnCloseAddKegiatan" class="top-4 right-4 rounded-full p-1 bg-red-500 hover:bg-red-700 cursor-pointer">
+                            <x-css-close class="w-3 h-3" />
+                        </button>
+                    </div>
+                </div>
+
+                <form action="{{ route('admin.Kegiatan.store') }}" method="POST" class="flex flex-col gap-4" enctype="multipart/form-data">
+                    @csrf
+                    
+                    <div class="flex flex-col w-full gap-5 pt-2">
+    
+                        <div class="grid grid-cols-1 md:grid-cols-4 items-center gap-2">
+                            <label for="judul" class="text-sm font-semibold text-gray-800">
+                                Judul
+                            </label>
+    
+                            <input type="text" name="judul" required id="judul" placeholder="Masukkan Judul Kegiatan" class="md:col-span-3 w-full rounded-md text-black border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+                        </div>
+    
+                        <div class="grid grid-cols-1 md:grid-cols-4 items-start gap-2">
+                            <label for="gambar" class="text-sm font-semibold text-gray-800 pt-2">
+                                Image
+                            </label>
+    
+                               <div class="md:col-span-3">
+                                    <input type="file" name="gambar" required id="gambar" accept="image/png,image/jpeg,image/jpg,image/webp" class="w-full rounded-md text-sm text-gray-700 border border-gray-300 bg-gray-100 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-blue-500 file:text-white hover:file:bg-blue-600 focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+
+                                    @error('gambar')
+                                        <span class="text-sm text-red-500">{{ $message }}</span>
+                                    @enderror
+
+                                    <p class="mt-1 text-xs text-gray-500">
+                                        Format: JPG, JPEG, PNG, atau WEBP. Ukuran 520x320. Maksimal 2 MB.
+                                    </p>
+                                </div>
+                        </div>
+
+                        <div class="grid grid-cols-1 md:grid-cols-4 items-center gap-2">
+                            <label for="deskripsi" class="text-sm font-semibold text-gray-800">
+                                Deskripsi
+                            </label>
+    
+                            <textarea cols="4" rows="2" name="deskripsi" required id="deskripsi" placeholder="Masukkan Deskripsi Kegiatan" class="md:col-span-3 w-full rounded-md text-black border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200"></textarea>
+                        </div>
+    
+                        <div class="grid grid-cols-1 md:grid-cols-4 items-start gap-2">
+                            <label for="tanggal" class="text-sm font-semibold text-gray-800 pt-2">
+                                Tanggal
+                            </label>
+    
+                           <input type="date" name="tanggal" required id="tanggal" placeholder="Masukkan Tanggal Kegiatan" class="md:col-span-3 w-full rounded-md text-black border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+                        </div>
+    
+                    </div>
+    
+                    <div class="flex w-full h-full justify-end items-end">
+                        <button type="submit" class="flex justify-center items-center p-2 rounded-md bg-green-500 hover:bg-green-700 shadow-md cursor-pointer">
+                            Tambah
+                        </button>
+                    </div>
+                </form>
+            </div>
+            
+        </article>
+        
+    @endif
+    {{-- overlay Add Kegiatan --}}
+
+    {{-- overlay Edit Kegiatan --}}
+    @if ($overlayEditKegiatan)
+        <article class="absolute flex top-0 left-0 items-center justify-center w-full h-full bg-gray-400/60 z-50">
+            <div class="flex flex-col w-fit h-fit gap-4 p-4 bg-white rounded-md">
+                
+                <div class="flex w-full h-fit gap-1 justify-between items-center bg-gray-100 rounded-md p-2">
+                    <div class="flex w-full h-auto gap-1 items-center">
+                        <h1 class="font-semibold text-base text-black capitalize">Edit Kegiatan</h1>
+                    </div>
+                    <div class="flex w-[30%] h-auto gap-1 justify-end items-center">
+                        <button type="button" wire:click="btnCloseEdit" class=" top-4 right-4 rounded-full p-1 bg-red-500 hover:bg-red-700 cursor-pointer">
+                            <x-css-close class="w-3 h-3" />
+                        </button>
+                    </div>
+                </div>
+
+                <form wire:submit.prKegiatan="updateKegiatan" class="flex flex-col gap-4">
+                    @csrf
+                    
+                    <div class="flex flex-col w-full gap-5 pt-2">
+    
+                        <div class="grid grid-cols-1 md:grid-cols-4 items-center gap-2">
+                            <label for="judul" class="text-sm font-semibold text-gray-800">
+                                Judul
+                            </label>
+    
+                            <input type="text" wire:model="judul" name="judul" required id="judul" placeholder="Masukkan Judul Kegiatan" class="md:col-span-3 w-full rounded-md text-black border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+                        </div>
+    
+                        <div class="grid grid-cols-1 md:grid-cols-4 items-start gap-2">
+                            <label for="gambar" class="text-sm font-semibold text-gray-800 pt-2">
+                                Image
+                            </label>
+    
+                               <div class="md:col-span-3">
+                                    <input type="file" name="gambar" wire:model="gambar" id="gambar" accept="image/png,image/jpeg,image/jpg,image/webp" class="w-full rounded-md text-sm text-gray-700 border border-gray-300 bg-gray-100 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-blue-500 file:text-white hover:file:bg-blue-600 focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+
+                                    @error('gambar')
+                                        <span class="text-sm text-red-500">{{ $message }}</span>
+                                    @enderror
+
+                                    @if ($currentImage)
+                                        <img src="{{ asset('storage/' . $currentImage) }}" class="w-28 h-20 object-cover rounded-md">
+                                    @endif
+                                    <p class="mt-1 text-xs text-gray-500">
+                                        Format: JPG, JPEG, PNG, atau WEBP. Ukuran 520x320. Maksimal 2 MB.
+                                    </p>
+                                </div>
+                        </div>
+
+                        <div class="grid grid-cols-1 md:grid-cols-4 items-center gap-2">
+                            <label for="deskripsi" class="text-sm font-semibold text-gray-800">
+                                Deskripsi
+                            </label>
+    
+                            <textarea cols="4" rows="2" wire:model="deskripsi" name="deskripsi" required id="deskripsi" placeholder="Masukkan Deskripsi Kegiatan" class="md:col-span-3 w-full rounded-md text-black border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200"></textarea>
+                        </div>
+    
+                        <div class="grid grid-cols-1 md:grid-cols-4 items-start gap-2">
+                            <label for="tanggal" class="text-sm font-semibold text-gray-800 pt-2">
+                                Tanggal
+                            </label>
+    
+                           <input type="date" wire:model="tanggal" name="tanggal" required id="tanggal" placeholder="Masukkan Tanggal Kegiatan" class="md:col-span-3 w-full rounded-md text-black border border-gray-300 bg-gray-100 px-3 py-2 text-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+                        </div>
+    
+                    </div>
+    
+                    <div class="flex w-full h-full justify-end items-end">
+                        <button type="submit" class="flex justify-center items-center p-2 rounded-md bg-green-500 hover:bg-green-700 shadow-md cursor-pointer">
+                            Edit
+                        </button>
+                    </div>
+                </form>
+            </div>
+            
+        </article>
+        
+    @endif
+    {{-- overlay Edit Kegiatan --}}
+
+    {{-- notifikasi Add --}}
+    @if (session('addSuccess'))
+        <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show" x-transition.duration.500ms class="absolute top-2 right-6 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4" role="alert">
+            <span class="block sm:inline">{{ session('addSuccess') }}</span>
+        </div>
+    @endif
+
+    @if (session('addGagal'))
+        <div class="absolute top-2 right-6 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4" role="alert">
+            <span class="block sm:inline">{{ session('addGagal') }}</span>
+        </div>
+    @endif
+    {{-- notifikasi Add --}}
+    
+    {{-- notifikasi delete --}}
+    @if ($deleteSuccess)
+        <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show" x-transition.duration.500ms class="absolute top-2 right-6 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4" role="alert">
+            <span class="block sm:inline">{{ $deleteSuccess }}</span>
+        </div>
+    @endif
+
+    @if ($deleteGagal)
+        <div class="absolute top-2 right-6 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4" role="alert">
+            <span class="block sm:inline">{{ $deleteGagal }}</span>
+        </div>
+    @endif
+    {{-- notifikasi delete --}}
+
+    {{-- notifikasi Edit --}}
+    @if ($editSuccess)
+        <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show" x-transition.duration.500ms class="absolute top-2 right-6 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4" role="alert">
+            <span class="block sm:inline">{{ $editSuccess }}</span>
+        </div>
+    @endif
+
+    @if ($editGagal)
+        <div class="absolute top-2 right-6 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4" role="alert">
+            <span class="block sm:inline">{{ $editGagal }}</span>
+        </div>
+    @endif
+    {{-- notifikasi Edit --}}
 
 </section>
