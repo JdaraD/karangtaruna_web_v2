e99b59be2b1e606c3cfb292eb7f53827f366a6a4
Julian Diwandara <juliandara17@gmail.com>
2026-09-08 13:39:34 +0700

    update dashboard

--- resources/views/pages/admin/⚡dashboard.blade.php
+++ resources/views/pages/admin/⚡dashboard.blade.php
@@ -1,14 +1,46 @@
 <?php
 
 use Livewire\Component;
+use App\Models\identity;
+use App\Models\product;
+use App\Models\kegiatan;
+use App\Models\kolaborasi;
+
 
 new class extends Component
 {
+    public $identity, $product, $kegiatan, $kolaborasi;
 
     // load data
+    public function loadIdentity()
+    {
+        $this->identity = identity::latest()->take(1)->get();
+    }
+
+    public function countProduct()
+    {
+        $this->product = product::count();
+    }
+
+    public function countKegiatan()
+    {
+        $this->kegiatan = kegiatan::count();
+    }
+
+    public function countKolaborasi()
+    {
+        $this->kolaborasi = kolaborasi::count();
+    }
     // load data
 
     // function mount
+    public function mount()
+    {
+        $this->loadIdentity();
+        $this->countProduct();
+        $this->countKegiatan();
+        $this->countKolaborasi();
+    }
     // function mount
 
     // function Button
@@ -41,17 +73,53 @@ new class extends Component
     </article>
 
     <article class="flex flex-none gap-4 items-center justify-between w-full">
-        @for ($i = 1; $i <= 3; $i++)
-        <div class="flex w-full lg:h-34 md:h-30 h-18 bg-gray-300 animate-pulse shadow-lg"></div>
-        @endfor
+        <div class="flex justify-between items-center w-full lg:h-34 md:h-30 h-18 px-4 bg-white rounded-md shadow-md">
+            <p class="text-xl font-semibold text-black">Usaha Mandiri : {{ $product }} product</p>
+            <a href="{{ route('admin.usaha') }}" class="flex justify-center items-center">
+                <x-gmdi-business-center class="h-10 w-10 text-black " />
+            </a>
+        </div>
+
+        <div class="flex justify-between items-center w-full lg:h-34 md:h-30 h-18 px-4 bg-white rounded-md shadow-md">
+            <p class="text-xl font-semibold text-black">Kegiatan : {{ $kegiatan }}</p>
+            <a href="{{ route('admin.kegiatan') }}" class="flex justify-center items-center">
+                <x-bi-activity class="h-10 w-10 text-black"/>
+            </a>
+        </div>
+        <div class="flex justify-between items-center w-full lg:h-34 md:h-30 h-18 px-4 bg-white rounded-md shadow-md">
+            <p class="text-xl font-semibold text-black">Kolaborasi : {{ $kolaborasi }}</p>
+            <a href="{{ route('admin.kolaborasi') }}" class="flex justify-center items-center">
+                <x-iconpark-cooperativehandshake-o class="h-10 w-10 text-black"/>
+            </a>
+        </div>
     </article>
 
     <article class="flex flex-none gap-4 items-center w-full">
         <div class="flex justify-center items-center w-[64%] h-80 bg-gray-300 animate-pulse shadow-lg">
             <p class="text-black">statistik view</p>
         </div>
-        <div class="flex justify-center items-center w-[36%] h-80 bg-gray-300 animate-pulse shadow-lg">
-            <p class="text-black">profile perusahan</p>
+        <div wire:poll.1s class="flex flex-col justify-stretch items-center lg:w-[36%] w-full gap-2 lg:h-80 h-auto p-4 bg-white rounded-md shadow-md">
+            <div class="flex w-full h-auto gap-1 justify-between items-center bg-gray-100 rounded-md p-2">
+                <div class="flex w-full h-auto gap-1 items-center">
+                    <h1 class="font-semibold text-base text-black capitalize">Identitas</h1>
+                </div>
+            </div>
+            @foreach ($identity as $it )
+                <div class="flex justify-center items-center">
+                    <img src="{{ asset('storage/'. $it->image) }}" alt="" class="w-40 h-42 rounded-md object-cover">
+                </div>
+                <div class="flex flex-col items-center">
+                    <div class="flex items-center gap-1">
+                        <h1 class="font-semibold capitalize lg:text-lg md:text-base text-base text-black">Nama Organiasi :</h1>
+                        <p class="lg:text-base text-sm text-black">{{ $it->name }}</p>
+                    </div>
+
+                    <div class="flex items-center gap-1">
+                        <h1 class="font-semibold capitalize lg:text-base text-sm text-black">Periode : </h1>
+                        <p class="lg:text-sm text-xs text-black"> {{ $it->periode }}</p>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </article>
 
